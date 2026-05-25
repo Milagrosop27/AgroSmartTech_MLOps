808f0b43405149ad58490420adb936b2960e5ded
mj <mjcarrillo0621@outlook.com>
2026-05-25 18:44:44 -0500

    Eliminar archivos .pkl del rastreo y actualizar gitignore

---
+++ frontend/src/views/guardian.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const alertsia= () => {
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-bold text-gray-800">Panel General (Overview)</h1>
+      <p>Aquí irán las tarjetas de resumen global.</p>
+    </div>
+  );
+};
+
+export default alertsia;
